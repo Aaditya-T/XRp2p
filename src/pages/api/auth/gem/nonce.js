ad618cc9c7d822f24c4e2fac2af594fbe00cd668
Aaditya-T <70804657+Aaditya-T@users.noreply.github.com>
2024-04-01 18:42:57 +0530

    Update nonce.js

--- src/pages/api/auth/gem/nonce.js
+++ src/pages/api/auth/gem/nonce.js
@@ -5,6 +5,7 @@ export default async function handler(req, res) {
         const pubkey = req.query.pubkey;
         const address = req.query.address;
         const token = jwt.sign({ public_key: pubkey, address: address }, process.env.ENC_KEY, { expiresIn: "1h" });
+        res.setHeader('Access-Control-Allow-Origin','*');
         res.status(200).json({ token: token });
     } catch (error) {
         res.status(400).json({ error: error.message });
