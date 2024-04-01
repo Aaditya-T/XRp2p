b08a5e5b14c7e1eab4a86761d23d628bc066e1e3
Aaditya-T <70804657+Aaditya-T@users.noreply.github.com>
2024-04-01 18:45:35 +0530

    Update hash.js

--- src/pages/api/auth/crossmark/hash.js
+++ src/pages/api/auth/crossmark/hash.js
@@ -13,6 +13,7 @@ function generateSecureRandomHash() {
 export default async function handler(req, res) {
     try {
         const hash = generateSecureRandomHash();
+        res.setHeader('Access-Control-Allow-Origin','*');
         res.status(200).json({hash:hash});
     } catch (error) {
         res.status(400).json({error:error.message});
